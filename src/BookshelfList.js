60d3e998a437fe63dc74b4967a672345a8d1c55a
Eric Seidler <eric.seidler@asynchrony.com>
2018-01-26 20:30:37 -0600

    refactor: use React Router to display main page and search page

--- src/BookshelfList.js
+++ src/BookshelfList.js
@@ -3,10 +3,19 @@ import Bookshelf from './Bookshelf'
 
 function BookshelfList({ bookshelves, books }) {
   return (
-    <div>
-      {bookshelves.map(bookshelf => (
-        <Bookshelf key={bookshelf} title={bookshelf} books={books.filter(book => book.bookshelf === bookshelf)} />
-      ))}
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div>
+        {bookshelves.map(bookshelf => (
+          <Bookshelf key={bookshelf} title={bookshelf} books={books.filter(book => book.bookshelf === bookshelf)} />
+        ))}
+      </div>
+      <div className="open-search">
+        {/* TODO: replace with link to Add/Search page */}
+        <a onClick={() => {}}>Add a book</a>
+      </div>
     </div>
   )
 }
