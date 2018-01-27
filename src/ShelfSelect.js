7255afad2e69c652910288e8864b4c5f40acc7fb
Eric Seidler <eric.seidler@asynchrony.com>
2018-01-26 19:22:21 -0600

    refactor: create ShelfSelector component to remove duplication

---
+++ src/ShelfSelect.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ShelfSelect() {
+  return (
+    <select>
+      <option value="none" disabled>Move to...</option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  )
+}
+
+export default ShelfSelect
