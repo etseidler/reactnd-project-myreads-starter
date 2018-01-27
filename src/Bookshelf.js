cc23e63324135a98f49b71d1e72fe9193af69465
Eric Seidler <eric.seidler@asynchrony.com>
2018-01-26 19:59:38 -0600

    refactor: create Bookshelf component and extract book data to an array

---
+++ src/Bookshelf.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Book from './Book'
+
+function Bookshelf({ title, books }) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(book => (
+            <li key={book.title}>
+              <Book title={book.title} author={book.author} imageURL={book.imageURL} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Bookshelf
