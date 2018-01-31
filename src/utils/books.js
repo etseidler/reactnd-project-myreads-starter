1006f86080990b3a18479b5068c327ebe9da7a16
Eric Seidler <eric.seidler@asynchrony.com>
2018-01-31 08:21:49 -0600

    fix: handle multiple authors in a better way

--- src/utils/books.js
+++ src/utils/books.js
@@ -1,8 +1,9 @@
 export const normalizeBooks = (books) => {
+  const multipleAuthorReducer = (acc, curr) => `${acc} and ${curr}`
   return books.map(({ id, title, authors, imageLinks, shelf }) => ({
     id,
     title,
-    author: authors ? authors[0] : 'UNKNOWN',
+    author: authors ? authors.reduce(multipleAuthorReducer) : 'UNKNOWN',
     imageURL: imageLinks.thumbnail,
     bookshelf: shelf
   }))
