b375eeb8647fedb887b781ddecf635429c71bc3a
Eric Seidler <eric.seidler@asynchrony.com>
2018-01-31 08:38:50 -0600

    feat: hide "none" option on search results page

--- src/ShelfSelect.js
+++ src/ShelfSelect.js
@@ -23,7 +23,10 @@ class ShelfSelect extends Component {
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
-        <option value="none">None</option>
+        {this.state.bookshelf !== 'none' ?
+          <option value="none">None</option> :
+          null
+        }
       </select>
     )
   }
